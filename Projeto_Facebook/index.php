eda3150e4f44ca43ce435607dbdc697480e22215
Marcos Vinícius de Mello <marcosvinimello@gmail.com>
2023-03-06 14:35:00 -0300

    Update index.php
    Inclusão da lista de meses no select da data de nascimento e do for no select da lista de anos.

--- Projeto_Facebook/index.php
+++ Projeto_Facebook/index.php
@@ -77,10 +77,25 @@
 							<?php } ?>
 						</select><!--nascimento-->
 						<select name="nascimento-mes" class="nascimento">
-							<option value="0">Junho</option>
+							<option value="1">Janeiro</option>
+							<option value="2">Fevereiro</option>
+							<option value="3">Março</option>
+							<option value="4">Abril</option>
+							<option value="5">Maio</option>
+							<option value="6">Junho</option>
+							<option value="7">Julho</option>
+							<option value="8">Agosto</option>
+							<option value="9">Setembro</option>
+							<option value="10">Outubro</option>
+							<option value="11">Novembro</option>
+							<option value="12">Dezembro</option>
 						</select><!--nascimento-->
 						<select name="nascimento-ano" class="nascimento">
-							<option value="0">1999</option>
+							<?php
+								for($i = 1960; $i <= 2023; $i++){
+							?>
+							<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+							<?php } ?>
 						</select><!--nascimento-->
 						<div class="clear"></div>
 					</div><!--w100-->
